9ce093257a469899dc7691aaffef8176dcb449ba
hwangkwang <sjjongg123@gmail.com>
2017-09-19 10:38:19 +0900

    commit common

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
@@ -4,7 +4,7 @@ public class HelloGit4 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		
+		System.out.println("Çª½¬ÇÏ°í½Í¾î¿æ,,");
 		// how are you doing??
 	}
 }
