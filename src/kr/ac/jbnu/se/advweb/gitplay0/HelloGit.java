74f53d0bb226c92914ff79e66086ba968ec3e081
ParkYeonHo <whwlsvy12@naver.com>
2017-09-19 10:37:43 +0900

    ㅎㅎ

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit.java
@@ -1,21 +1,19 @@
 package kr.ac.jbnu.se.advweb.gitplay0;
 
-// 201211219 software 박연호
+// 201211219 software 박연호 
 public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		// how are you doing??
 		System.out.println("Hello Git!!???????????");
-		
+
 		// Okay I'll change this part as below:
 		// I've just added the following part in Eclipse
-		for(int i = 0;i < 100; i++)
-		{
+		for (int i = 0; i < 100; i++) {
 			System.out.println("i:" + i);
 		}
-		
-		
+
 		// I added the following part.
 		System.out.println("This is a confliction test.");
 	}
--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit2.java
@@ -4,7 +4,7 @@ public class HelloGit2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		
+		//asd
 		// how are you doing??
 	}
 }
